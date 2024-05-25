2f87f701c5f8bbce83ab51ddb3f8d7a82b8a824b
Maria <ronios@yandex.ru>
2024-05-25 23:50:43 +0500

    [Задание №2] Добавлена опция запуска поиска самого длинного слова в массиве.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,16 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class Calculator {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static Scanner scanner;
 
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+
+        System.out.println("Введите номер задания:\n1 - Калькулятор\n2 - Вывод самой длинной строки в массиве");
+
+        while (!scanner.hasNext("[1-2]")) {
+            System.out.println("Некорректный ввод! Введите корректный номер задания.");
+            scanner.next();
+        }
+        String task = scanner.next();
+
+        switch (task) {
+            case "1":
+                startCalculator();
+                break;
+            case "2":
+                startLongestWordSearcher();
+                break;
+        }
+        scanner.close();
+    }
+
+    private static void startCalculator() {
         System.out.println("Введите число a, число может быть дробным:");
-        float a = getNextFloat(scanner);
+        float a = getNextFloat();
 
         System.out.println("Введите число b, число может быть дробным:");
-        float b = getNextFloat(scanner);
+        float b = getNextFloat();
 
         System.out.println("Введите номер операции, которую хотите выполнить:\n1 - Сложение\n2 - Вычитание\n3 - Деленение\n4 - Умножение");
 
@@ -34,15 +59,30 @@ public class Calculator {
                 System.out.printf("Результат операции a * b: %.4f", a * b);
                 break;
         }
-        scanner.close();
+    }
+
+    private static void startLongestWordSearcher() {
+        System.out.println("Введите размер массива.");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Некорректный ввод! Введите целое число для задания размера массива");
+            scanner.next();
+        }
+        int arraySize = scanner.nextInt();
+        List<String> values = new ArrayList<>(arraySize);
+
+        for (int i = 0; i < arraySize; i++) {
+            System.out.printf("Введите строку для добавления в массив. Осталось ввести %d строк(и)\n", arraySize - i);
+            values.add(scanner.next());
+        }
+        Collections.sort(values);
+        System.out.printf("Самая длинна строка в списке - %s", values.get(arraySize - 1));
     }
 
     /**
      * Метод для получения следующего float числа из консоли.
      * При вводе любого значения, не являющегося float числом, будет выведено сообщение и ввод необходимо повторить.
-     * @param scanner - текущий сканер для считывания числа из консоли.
      */
-    private static float getNextFloat(Scanner scanner) {
+    private static float getNextFloat() {
         while (!scanner.hasNextFloat()) {
             System.out.println("Некорректный ввод! Введите новое число.");
             scanner.next();
